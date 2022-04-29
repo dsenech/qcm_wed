e46af88b252e826e5cffb67d610909f81f5d63d5
David Sénéchal <david.senechal@usherbrooke.ca>
2022-04-29 18:31:54 -0400

    fixed bug in setup.py for obtaining the version number (tag of current version).

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ import skbuild
 import os
 import subprocess
 git_hash = str(subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']))
-# version = str(subprocess.check_output(['git', 'describe', '--exact-match', '--tags']))
+version = str(subprocess.check_output(['git', 'describe', '--tags']))
 git_hash = git_hash[2:-3]
-# version = version[2:-3]
-version = 'NA'
+version = version[2:-3]
+version = version.rsplit("-")[0]
 
 fout = open("pyqcm/qcm_git_hash.py", "w")
 fout.write("git_hash = '{:s}'\n".format(git_hash))
